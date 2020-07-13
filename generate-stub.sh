6070d38e4c9ca03369370e9187ca480cf16a34c7
EdwinBetanc0urt <EdwinBetanc0urt@hotmail.com>
2020-07-13 12:09:47 -0400

    other changes.

--- generate-stub.sh
+++ generate-stub.sh
@@ -1,5 +1,5 @@
 #####################################################################################
-# Product: ADempiere Install protoc                                                 #
+# Product: ADempiere Generate Stub                                                  #
 # Copyright (C) 2012-2020 E.R.P. Consultores y Asociados, C.A.                      #
 # Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                      #
 # This program is free software: you can redistribute it and/or modify              #
